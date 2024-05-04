1091c8b84181cd0fce38b0f9c5f5a78634a434c2
Wojciech <kowalczyk.wojtek168@gmail.com>
2024-05-04 14:50:36 +0200

    fixed bug - zad2 done

--- Company.java
+++ Company.java
@@ -11,7 +11,7 @@ public class Company {
         employees[3] = new Employee("Marta Wiśniewska", 4100.00);
         employees[4] = new Worker("Krzysztof Lewandowski", 4900.00, "Data Analyst");
 
-        System.out.println("\nDane wszystkich pracowników:");
+        System.out.println("\nData of all employees:");
         for (Employee employee : employees) {
             System.out.println(employee);
         }
